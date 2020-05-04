518f51815c73d96634d7ece8eb2f38d60646093c
Thomas Kaliakos <tkaliakos@gmail.com>
2020-05-04 01:26:29 +0100

    v1: Created 1st procedural, buggy solution

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+## Functional Programming Concepts
 Here you will find the code and the slides used for the Functional Programming Concepts session.
 
-Talk abstract and description can be consulted [here](./ABSTRACT_EN.md) (EN).
+Talk abstract and description can be consulted [here](./ABSTRACT_EN.md).
 
 Slides can be found directly on the [companion site](https://thomaska.github.io/fp-concepts-in-scala/).
+
+### Tic Tac Toe
+This is the traditional Tic Tac Toe game, played in the command line.<br>
+The user enters his input by providing the coordinates of the square he wants to play.<br>
+The computer plays by selecting a random square.
--- src/tictactoe/Main.scala
+++ src/tictactoe/Main.scala
@@ -1,6 +1,134 @@
 package tictactoe
 
-object Main extends App {
+import scala.io.StdIn
+import scala.util.Random
 
-  println("Hello world")
+/**
+* Symptoms:
+ * - Sometimes the game crashes in the main menu
+ * - Sometimes the game crashes during play
+ * - Sometimes the computer takes quite some time to play
+ * - No testing
+ */
+object Main extends App {
+  var grid = Array.fill(3, 3)("")
+  var loop = true
+
+  while (loop) {
+    println("Press N for new game, or Q to quit:")
+    val input = StdIn.readLine
+    input match {
+      case "Q" =>
+        System.exit(1)
+      case "N" =>
+        printGrid
+        loop = false
+    }
+  }
+
+  var somebodyWon = false
+  var draw = false
+
+  while (!somebodyWon && !draw) {
+    println(
+      "You are player 'X'. Enter the square you want to play in the format: <row> <column>. Eg \"1 3\" is the top right square:"
+    )
+    val coord = StdIn.readLine().split(" ")
+    val x = coord(0).toInt - 1
+    val y = coord(1).toInt - 1
+    grid(x)(y) = "X"
+    printGrid
+
+    if (checkDraw()) {
+      println("The game ended in a draw!")
+      draw = true
+    }
+
+    if (playerWon("X")) {
+      println("Congratulations!! You win!")
+      somebodyWon = true
+    }
+
+    if (!somebodyWon && !draw) {
+      println("Computer plays:")
+      var computerIsPlaying = true
+      while (computerIsPlaying) {
+        val randomX = Random.nextInt(3)
+        val randomY = Random.nextInt(3)
+        if (grid(randomX)(randomY).isEmpty) {
+          grid(randomX)(randomY) = "O"
+          printGrid
+          computerIsPlaying = false
+        }
+      }
+      if (checkDraw()) {
+        println("The game ended in a draw!")
+        draw = true
+      }
+
+      if (playerWon("O")) {
+        println("Computer wins!")
+        somebodyWon = true
+      }
+    }
+  }
+
+  def playerWon(player: String): Boolean = {
+    var allTheSame = true
+    for (i <- 0 to 2) {
+      allTheSame = true
+      for (j <- 0 to 2) {
+        if (grid(i)(j) != player)
+          allTheSame = false
+      }
+      if (allTheSame)
+        return true
+    }
+
+    for (i <- 0 to 2) {
+      allTheSame = true
+      for (j <- 0 to 2) {
+        if (grid(j)(i) != player)
+          allTheSame = false
+      }
+      if (allTheSame)
+        return true
+    }
+
+    allTheSame = true
+    for (i <- 0 to 2) {
+      if (grid(i)(i) != player)
+        allTheSame = false
+    }
+    if (allTheSame)
+      return true
+    allTheSame = true
+    for (i <- 0 to 2) {
+      if (grid(2 - i)(i) != player)
+        allTheSame = false
+    }
+    allTheSame
+  }
+
+  def checkDraw(): Boolean = {
+    var thereIsEmptySquare = false
+    for (i <- 0 to 2) {
+      for (j <- 0 to 2) {
+        if (grid(i)(j).isEmpty) {
+          thereIsEmptySquare = true
+        }
+      }
+    }
+    !thereIsEmptySquare
+  }
+
+  def printGrid: Unit = {
+    for (i <- 0 to 2) {
+      if (i != 0) {
+        println("------------------------")
+      }
+      println(s"\t${grid(i)(0)}\t|\t${grid(i)(1)}\t|\t${grid(i)(2)}")
+    }
+    println("\n")
+  }
 }
